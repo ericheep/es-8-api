19005b8ecf286f524fac36916415dbf0362b7ed9
ericheep <ericheep@gmail.com>
2018-12-24 14:54:27 -0800

    :fire: change from vue-cli to webpack
    Vue cli was giving `WDS Disconnect` errors on empty projects.

--- vue-es8-client/src/main.js
+++ vue-es8-client/src/main.js
@@ -1,15 +1,22 @@
 import Vue from 'vue'
-// import VueSocketIO from 'vue-socket.io'
 // import socketio from 'socket.io-client'
 import App from './App.vue'
 import { store } from './store/store'
 
-// export const SocketInstance = socketio('http://localhost:4113')
+// import VueSocketIO from 'vue-socket.io'
 
-// Vue.use(VueSocketIO, SocketInstance)
+// Vue.use(VueSocketIO, 'http://localhost:3128')
 Vue.config.productionTip = false
 
 new Vue({
+  // sockets: {
+  //   connect: function() {
+  //     console.log('connected')
+  //   },
+  //   customEmit: function() {
+  //     console.log('emit')
+  //   },
+  // },
   store: store,
   render: h => h(App),
 }).$mount('#app')
