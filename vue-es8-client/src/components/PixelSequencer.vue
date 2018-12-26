50016377cebc36a0e82e84157d8b6d7abe8aed2b
ericheep <ericheep@gmail.com>
2018-12-26 11:45:06 -0800

    :sparkles: add sequencer component

---
+++ vue-es8-client/src/components/PixelSequencer.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+
+export default {
+  name: 'PixelSequencer',
+  data: () => ({
+    bpm: ''
+  }),
+  computed: {
+  },
+  methods: {
+    ...mapActions([
+      'changeTempo'
+    ]),
+  },
+}
+</script>
+
+<style scoped>
+</style>
