10c60a41fc730cbf63d2b642c49cd9fa643472c2
ericheep <ericheep@gmail.com>
2018-12-25 16:30:34 -0800

    :sparkles: update for vue-socket.io-extended

--- vue-es8-client/src/main.js
+++ vue-es8-client/src/main.js
@@ -1,19 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
-import VueSocketIO from 'vue-socket.io'
 import { store } from './store/store'
+import VueSocketio from 'vue-socket.io-extended'
+import io from 'socket.io-client'
 
 Vue.config.productionTip = false
-
-Vue.use(new VueSocketIO({
-  debug: true,
-  connection: 'http://localhost:3000',
-  vuex: {
-    store,
-    actionPrefix: 'SOCKET_',
-    mutationPrefix: 'SOCKET_'
-  }
-}))
+Vue.use(VueSocketio, io('http://localhost:3000'), { store })
 
 new Vue({
   store: store,
