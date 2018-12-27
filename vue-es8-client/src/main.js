0f039b7a6353fd838ff9015ec8386283070d9922
ericheep <ericheep@gmail.com>
2018-12-27 00:47:16 -0800

    :art: clean up unused paper import

--- vue-es8-client/src/main.js
+++ vue-es8-client/src/main.js
@@ -1,17 +1,11 @@
 import Vue from 'vue'
-import VueResize from 'vue-resize'
-import 'vue-resize/dist/vue-resize.css'
 import App from './App.vue'
-import { store } from './store/store'
 import VueSocketio from 'vue-socket.io-extended'
 import io from 'socket.io-client'
-
-import paper from 'paper'
-Object.defineProperty(Vue.prototype, '$paper', { value: paper })
+import { store } from './store/store'
 
 Vue.config.productionTip = false
 Vue.use(VueSocketio, io('http://localhost:3000'), { store })
-Vue.use(VueResize)
 
 new Vue({
   store: store,
