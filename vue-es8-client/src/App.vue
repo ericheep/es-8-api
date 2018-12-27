2ddbe609f084a3e2790f18e4fc5c6ff420e8156a
ericheep <ericheep@gmail.com>
2018-12-27 00:46:47 -0800

    :sparkles: add topbar

--- vue-es8-client/src/App.vue
+++ vue-es8-client/src/App.vue
@@ -1,27 +1,55 @@
 <template>
   <div id="app">
-    <Sequencer/>
+    <Topbar id="topbar"/>
+    <Sequencer id="sequencer"/>
   </div>
 </template>
 
 <script>
+import Topbar from './components/Topbar.vue'
 import Sequencer from './components/Sequencer.vue'
 
 export default {
   name: 'app',
   components: {
-    Sequencer
+    Topbar,
+    Sequencer,
   }
 }
 </script>
 
 <style>
+* {
+  border-style: dashed;
+}
+html, body, #app {
+  height: 100%;
+  margin: 0px;
+  border: none;
+}
+div {
+  margin: 0px;
+  border: none;
+}
 #app {
+  display: flex;
+  flex-direction: column;
+
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+#topbar {
+  min-height: 200px;
+  width: 100%;
+  border-style: dotted;
+}
+#sequencer {
+  min-height: 100%;
+  width: 100%;
+  border-style: dotted;
 }
 </style>
