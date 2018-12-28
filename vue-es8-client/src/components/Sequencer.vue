8acf9be3f591fccbe777c39eba50ce8781365cf7
ericheep <ericheep@gmail.com>
2018-12-27 22:39:24 -0800

    :art: :bug: fix updating issue by utilizing a watcher

--- vue-es8-client/src/components/Sequencer.vue
+++ vue-es8-client/src/components/Sequencer.vue
@@ -12,13 +12,20 @@ import paper from 'paper'
 //   return 12 * Math.log(freq / 440.0) / Math.log(2) + 69
 // }
 
+const drawSequencer = (sequence) => {
+  const div = document.getElementById('sequencer')
+  const width = div.clientWidth
+  const height = div.clientHeight
+
+  const path = new paper.Path.Rectangle(0, 0, width, height)
+  path.strokeColor = 'black'
+}
+
 export default {
   name: 'Sequencer',
-  data: () => ({
-  }),
   computed: {
     ...mapGetters([
-      'averagedSequence'
+      'sequence'
     ]),
   },
   methods: {
@@ -26,36 +33,16 @@ export default {
       'changeSequence'
     ]),
   },
+  watch: {
+    sequence: (s) => {
+      drawSequencer(s)
+    }
+  },
   mounted() {
     paper.install(window)
     window.onload = () => {
       paper.setup('sequencer')
     }
-
-    console.log('mounted', this.averagedSequence(100))
-  },
-  created() {
-    console.log('created', this.averagedSequence(100))
-  },
-  beforeUpdate() {
-    console.log('beforeUpdate', this.averagedSequence(100))
-  },
-  updated() {
-    console.log('updated', this.averagedSequence(100))
-
-    // const freqRange = [1.0, 24000.0]
-    // frequencyToPitch(freqRange[0])
-
-    // const sequence = this.$store.getters['sequence']
-    // const sequenceLength = this.$store.getters['sequence']
-    // console.log(sequence, sequenceLength)
-
-    // const div = document.getElementById('sequencer')
-    // const width = div.clientWidth
-    // const height = div.clientHeight
-
-    // const path = new paper.Path.Rectangle(0, 0, width, height)
-    // path.strokeColor = 'black'
   }
 }
 </script>
