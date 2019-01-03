81b4fa95ad1480290217a1768945937bcd6e3716
ericheep <ericheep@gmail.com>
2019-01-03 01:47:46 -0800

    :boom: :construction: add helper to convert freq to note names

--- vue-es8-client/src/helpers.js
+++ vue-es8-client/src/helpers.js
@@ -1,3 +1,12 @@
-export const frequencyToPitch = (freq) => {
+const frequencyToPitch = (freq) => {
   return 12 * Math.log(freq / 440.0) / Math.log(2) + 69
 }
+
+const frequencyToNote = (freq) => {
+  const notes = ['A', 'Bb', 'B', 'C', 'Db', 'D', 'Eb', 'E', 'F', 'F#', 'G', 'Ab']
+  const pitch = frequencyToPitch(freq)
+
+  return pitch
+}
+
+console.log(frequencyToNote(440))
