17b8018489e045c3db5675078068a3845429edae
ericheep <ericheep@gmail.com>
2019-01-05 19:59:10 -0800

    :wrench: update to new pitch standard

--- vue-es8-client/src/helpers.js
+++ vue-es8-client/src/helpers.js
@@ -1,47 +1,34 @@
-export const frequencyToPitch = (freq) => {
+export const frequencyToMIDIPitch = (freq) => {
   return 12 * Math.log(freq / 440.0) / Math.log(2) + 69
 }
 
-export const pitchToFrequency = (pitch) => {
+export const MIDIpitchToFrequency = (pitch) => {
   return 440 * Math.pow(2, (pitch - 69) / 12)
 }
 
-export const frequencyToNote = (freq) => {
-  const latins = ['A', 'Bb', 'B', 'C', 'Db', 'D', 'Eb', 'E', 'F', 'F#', 'G', 'Ab']
-  const pitch = frequencyToPitch(freq)
-  let cents = ((pitch % 1) * 100).toFixed(4)
+export const frequencyToPitch = (freq) => {
+  const pitchClasses = ['A', 'Bb', 'B', 'C', 'Db', 'D', 'Eb', 'E', 'F', 'F#', 'G', 'Ab']
+  const MIDIpitch = frequencyToMIDIPitch(freq)
+  let cents = ((MIDIpitch % 1) * 100).toFixed(4)
   let pitchAdjust = 0
 
   if (cents >= 50) {
     cents = -(100 - cents)
     pitchAdjust = 1
   }
-  const octave = Math.floor((pitch + pitchAdjust - 12) / 12)
-  const latin = latins[Math.floor((pitch + pitchAdjust - 9) % 12)]
+  const octave = Math.floor((MIDIpitch + pitchAdjust - 12) / 12)
+  const pitchClass = pitchClasses[Math.floor((MIDIpitch + pitchAdjust - 9) % 12)]
 
   return {
-    latin,
+    pitchClass,
     octave,
     cents,
   }
 }
 
-export const noteToFrequency = (note) => {
-  const latins = [
-    ['C', 'B#'],
-    ['Db', 'C#'],
-    ['D'],
-    ['Eb', 'D#'],
-    ['E', 'Fb'],
-    ['F', 'E#'],
-    ['F#', 'Gb'],
-    ['G'],
-    ['Ab', 'G#'],
-    ['A'],
-    ['A#', 'Bb'],
-    ['B', 'Cb'],
-  ]
+export const pitchToFrequency = (pitch) => {
+  const pitchClasses = ['C', 'Db', 'D', 'Eb', 'E', 'F', 'F#', 'G', 'Ab', 'A', 'A#', 'B', 'Bb']
 
-  const pitch = latins.indexOf(latins.find((arr) => arr.find((el) => el === note.latin)))
-  return pitchToFrequency(pitch + note.cents + (note.octave * 12 + 12))
+  const pitchClassIndex = pitchClasses.indexOf(pitch.pitchClass)
+  return MIDIpitchToFrequency(pitchClassIndex + parseFloat(pitch.cents * 0.01) + (pitch.octave * 12 + 12))
 }
