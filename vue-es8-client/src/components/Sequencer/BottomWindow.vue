d7483cced6c52623ddc5ecfdbb01c1f773f7db69
ericheep <ericheep@gmail.com>
2019-01-08 23:00:51 -0800

    :sparkles: new component for non-important info below the sequencer

---
+++ vue-es8-client/src/components/Sequencer/BottomWindow.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="bottom-window">
+    Modified by 242.142.132.041 on 01/01/2019
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'BottomWindow',
+  computed: {
+    ...mapGetters([
+      'selectedSample',
+    ]),
+  },
+}
+</script>
+
+<style scoped>
+#bottom-window {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  width: 100%;
+}
+</style>
