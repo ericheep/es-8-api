6283d3a3360eebd3e10028e559a8aa53bfc4bdfd
ericheep <ericheep@gmail.com>
2019-01-12 12:20:02 -0800

    :sparkles: load vue-youtube dep

--- vue-es8-client/src/main.js
+++ vue-es8-client/src/main.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueSocketio from 'vue-socket.io-extended'
+import VueYoutube from 'vue-youtube'
 import io from 'socket.io-client'
 import { store } from './store/store'
 
 Vue.config.productionTip = false
 Vue.use(VueSocketio, io('http://localhost:3000'), { store })
+Vue.use(VueYoutube)
 
 new Vue({
   store,
