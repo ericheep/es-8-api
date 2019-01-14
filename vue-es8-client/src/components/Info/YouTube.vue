a14d2212fbe29654ff438f9039f0b09a3727a60d
ericheep <ericheep@gmail.com>
2019-01-13 23:04:23 -0800

    :sparkles: :building_construction: new components

---
+++ vue-es8-client/src/components/Info/YouTube.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="youTube">
+    <youtube video-id="j1eD_yUFfwk"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'YouTube',
+  data: () => ({
+    playerVars: {
+      autoplay: 1
+    },
+  }),
+}
+</script>
+
+<style scoped>
+#youTube {
+
+}
+</style>
