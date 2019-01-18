9c12dd0aefd556112aced164ab164cdafb1c7002
ericheep <ericheep@gmail.com>
2019-01-17 23:54:39 -0800

    :lipstick: create edge cases for drawing primed sample

--- vue-es8-client/src/paper/drawPrimedSample.js
+++ vue-es8-client/src/paper/drawPrimedSample.js
@@ -9,10 +9,24 @@ export default ({ primedSample, frequencyResponse, selectedArea, width, height }
   const offsetIndex = primedSample.index - selectedArea.startIndex
   const pitch = frequencyToMIDIPitch(primedSample.freq)
 
+  let size = [sampleWidth - 2, scale * 2]
+  let center = [sampleWidth * offsetIndex + sampleWidth / 2, height - ((pitch - pitchLo) * scale)]
+
+  if (offsetIndex === 0) {
+    size[0] -= 2
+    center[0] += 1
+  }
+
+  if (offsetIndex === selectedArea.samples.length - 1) {
+    size[0] -= 2
+    center[0] -= 1
+  }
+
   const selectedSampleRect = new paper.Path.Rectangle({
-    size: [sampleWidth - 2, scale * 2],
-    center: [sampleWidth * offsetIndex + sampleWidth / 2, height - ((pitch - pitchLo) * scale)],
+    size: size,
+    center: center,
     strokeWidth: 2,
   })
+  selectedSampleRect.sendToBack()
   selectedSampleRect.fillColor = 'pink'
 }
