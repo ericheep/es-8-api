888ecf5050b0e8f77019fcbadffb52b47b7d2a99
Eric <eric.heep@techmdinc.com>
2019-01-22 11:50:03 -0800

    :art: move socket methods into separate file

--- node-api-server/server.js
+++ node-api-server/server.js
@@ -1,17 +1,11 @@
 const http = require('http')
-const ipaddress = require('ip-address')
-const { formatTime, getRangesOfFrequencies, getInitialState } = require('./helpers')
-const { updateSample } = require('./osc')
+const { onConnection } = require('./socket')
 
 const server = http.createServer()
-const { sequencer, samples } = getInitialState()
-
-const allowedOrigins = "http://localhost:* http://127.0.0.1:*";
-
 const io = require('socket.io')(server, {
   path: '/',
   serveClient: false,
-  origins: allowedOrigins,
+  origins: 'http://localhost:* http://127.0.0.1:*',
 
   // below are engine.IO options
   pingInterval: 10000,
@@ -19,44 +13,5 @@ const io = require('socket.io')(server, {
   cookie: false
 })
 
-io.on('connect', (socket) => {
-  io.emit('INITIALIZE_SEQUENCER', sequencer)
-  io.emit('UPDATE_SELECTED_AREA', {
-    startIndex: 0,
-    endIndex: sequencer.samplesShown,
-    scopedIndex: 0,
-    samples: samples.slice(0, sequencer.samplesShown),
-  })
-  io.emit('UPDATE_TRANSPORT_RANGES',
-    getRangesOfFrequencies(samples.map((sample) => sample.freq), 760)
-  )
-  io.emit('UPDATE_UPTIME', formatTime(process.uptime() + ""))
-  const ipaddr = new ipaddress.Address4(socket.request.connection.remoteAddress)
-  console.log(ipaddr)
-
-  socket.on('emitCommitPrimedSample', (data) => {
-    const index = samples.findIndex((sample) => sample.index == data.index)
-    console.log(data)
-
-    samples[index] = {
-      freq: data.freq,
-      index: data.index,
-      dateTime: data.dateTime,
-    }
-
-    io.emit('UPDATE_COMMITTED_SAMPLE', data)
-    io.emit('UPDATE_TRANSPORT_RANGES',
-      getRangesOfFrequencies(samples.map((sample) => sample.freq), 760)
-    )
-  })
-
-  socket.on('emitSelectedArea', (data) => {
-    socket.emit('UPDATE_SELECTED_AREA', {
-      startIndex: data.startIndex,
-      endIndex: data.endIndex,
-      samples: samples.slice(data.startIndex, data.endIndex),
-    })
-  })
-})
-
+onConnection(io)
 server.listen(3000, '0.0.0.0')
---
+++ node-api-server/socket.js
@@ -0,0 +1,48 @@
+const { formatTime, getRangesOfFrequencies, getInitialState } = require('./helpers')
+const ipaddress = require('ip-address')
+const { updateSample } = require('./osc')
+const { sequencer, samples } = getInitialState()
+
+function onConnection(io) {
+  io.on('connect', (socket) => {
+    io.emit('INITIALIZE_SEQUENCER', sequencer)
+    io.emit('UPDATE_SELECTED_AREA', {
+      startIndex: 0,
+      endIndex: sequencer.samplesShown,
+      scopedIndex: 0,
+      samples: samples.slice(0, sequencer.samplesShown),
+    })
+    io.emit('UPDATE_TRANSPORT_RANGES',
+      getRangesOfFrequencies(samples.map((sample) => sample.freq), 760)
+    )
+    io.emit('UPDATE_UPTIME', formatTime(process.uptime() + ""))
+    const ipaddr = new ipaddress.Address4(socket.request.connection.remoteAddress)
+    console.log(ipaddr)
+
+    socket.on('emitCommitPrimedSample', (data) => {
+      const index = samples.findIndex((sample) => sample.index == data.index)
+      console.log(data)
+
+      samples[index] = {
+        freq: data.freq,
+        index: data.index,
+        dateTime: data.dateTime,
+      }
+
+      io.emit('UPDATE_COMMITTED_SAMPLE', data)
+      io.emit('UPDATE_TRANSPORT_RANGES',
+        getRangesOfFrequencies(samples.map((sample) => sample.freq), 760)
+      )
+    })
+
+    socket.on('emitSelectedArea', (data) => {
+      socket.emit('UPDATE_SELECTED_AREA', {
+        startIndex: data.startIndex,
+        endIndex: data.endIndex,
+        samples: samples.slice(data.startIndex, data.endIndex),
+      })
+    })
+  })
+}
+
+module.exports.onConnection = onConnection
